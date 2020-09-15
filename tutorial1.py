abf9b61866bcbe61ec9a19cb3f0fc5544ffe10d5
JLB423 <54405647+JLB423@users.noreply.github.com>
2020-09-15 12:51:16 -0400

    Update tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -4,10 +4,12 @@ a = 1
 
 b = (1,2,3,4,5)
 
+### PUSH FROM LOCAL TO GITHUB ###
+
 #	In the terminal make sure you are in the directory where your file that you want to push to git is located
 #	git init    # this initializes the git repository 
 #	git add filenamethatyouwanttoadd
-#	git commit –m “Created filename” –m “description goes here..”
+#	git commit –m “Created filename” –m “description goes here..”..
 #	**git push origin master   ##**** this will give an error since this file didn’t originate as a git repository
 # So, first we have to create an empty git repository on github 
 #	Copy the link from the repo
