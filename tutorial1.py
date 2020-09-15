c8336825de18e0fd820102d5704f7b312ab5514a
JLB423 <54405647+JLB423@users.noreply.github.com>
2020-09-15 12:45:50 -0400

    Update tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -4,6 +4,15 @@ a = 1
 
 b = (1,2,3,4,5)
 
+#	In the terminal make sure you are in the directory where your file that you want to push to git is located
+#	git init    # this initializes the git repository 
+#	git add filenamethatyouwanttoadd
+#	git commit –m “Created filename” –m “description goes here..”
+#	**git push origin master   ##**** this will give an error since this file didn’t originate as a git repository
+# So, first we have to create an empty git repository on github 
+#	Copy the link from the repo
+#	git remote add origin pastethelinkfromyourrepo
+#	git push origin master    # note in the future you don’t have to include origin master,  git push –u origin master   this will remember this moving forward. 
 
 
 
